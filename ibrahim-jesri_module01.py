d73f45de70e8cbc8e8a2aca5784e17396418210b
ijesri-school <46769617+ijesri-school@users.noreply.github.com>
2019-02-13 00:16:50 -0600

    Submission

---
+++ ibrahim-jesri_module01.py
@@ -0,0 +1,16 @@
+print('*')
+print('**')
+print('****')
+print('********')
+print('****************')
+
+'''
+	1. How many points did you score on the zyBooks assignment?
+		100%
+	2. What was the most difficult part of the assignment. 
+		Focusing on the work.
+	3. How long did it take to complete this assignment?
+		About three hours, but I was heavily distracted.
+	4. Did you complete your programming assignment and did it run as expected.
+		Yes
+'''
